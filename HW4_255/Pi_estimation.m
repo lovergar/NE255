9c5626f7c2af3d5685bd00ac6f40131ecbe71897
lovergar <31505758+lovergar@users.noreply.github.com>
2019-11-18 18:49:20 -0800

    Update Pi_estimation.m
    Added error bars

--- HW4_255/Pi_estimation.m
+++ HW4_255/Pi_estimation.m
@@ -19,7 +19,8 @@ Histories = [100 1000 10000]; % number of histories
 Abs_error=0*Histories;
 Rel_error=Abs_error;
 Pi_est=Abs_error;
-
+St_Dev=Abs_error;
+Rel_St_Dev=Abs_error;
 %% Calculation
 for i=1:length(Histories)
     % Sampling, calculation of arc(x), averaging and estimation of pi
@@ -28,6 +29,8 @@ for i=1:length(Histories)
     Pi_i = 4*arc(random_values);
     Pi_est(i)= sum(Pi_i)/N;
     % Calculation of the errors
+    St_Dev(i)=((1/(N*(N-1)))*sum((Pi_i-Pi_est(i)).^2))^0.5;
+    Rel_St_Dev(i)=St_Dev(i)/Pi_est(i);
     Abs_error(i)=abs(Pi_est(i) -Pi_real);
     Rel_error(i)=Abs_error(i)/Pi_real;
 end
@@ -38,7 +41,7 @@ txt3= 'Relative error';
 
 figure()
 subplot(3,1,1)
-plot(Histories, Pi_est,'.-','MarkerSize',20,'LineWidth',1,'DisplayName',txt1)
+errorbar(Histories, Pi_est,2*St_Dev,'.-','MarkerSize',20,'LineWidth',1,'DisplayName',txt1)
 hold on
 plot(Histories, Pi_real*[1,1,1],'--','LineWidth',1,'DisplayName','Pi real')
 xlabel('Number of histories');
